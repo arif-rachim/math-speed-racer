e842f4cda4a4f16407eb35375992dc3f513b957f
Developer 1002 <dev1002@otonetwork.co>
2020-12-24 20:31:22 +0400

    Initial commit

---
+++ src/comp/App.js
@@ -0,0 +1,119 @@
+import React, {useCallback, useMemo, useState} from "react";
+import Session from "./Session";
+import styles from "./App.module.css";
+
+const SPEED_RACER = 'SPEED_RACER';
+const DEFAULT_CONTEXT = {
+    config: {
+        totalSums: 30,
+        questionsEachSum: 4,
+        digits: 3,
+        negativePercentage: 0,
+        delayBetweenQuestionsInMilliseconds: 500
+    },
+    sessions: []
+};
+
+const onStartSession = (setStartSession) => () => {
+    setStartSession(true);
+}
+
+export const AppContext = React.createContext({});
+
+function isFunction(functionToCheck) {
+    return functionToCheck && {}.toString.call(functionToCheck) === '[object Function]';
+}
+
+function createAppContextPersist(oldAppContext, setAppContext) {
+    return (appContext) => {
+        let newContext = appContext;
+        if (isFunction(appContext)) {
+            newContext = appContext(oldAppContext);
+        }
+        setAppContext(newContext);
+        localStorage.setItem(SPEED_RACER, JSON.stringify(newContext));
+    }
+}
+
+export default function App() {
+
+    const [appContext, setAppContext] = useState(JSON.parse(localStorage.getItem(SPEED_RACER) || JSON.stringify(DEFAULT_CONTEXT)));
+    const setAppContextPersist = useCallback(createAppContextPersist(appContext, setAppContext), [appContext]);
+    const [startSession, setStartSession] = useState(false);
+    return <AppContext.Provider value={useMemo(() => [appContext, setAppContextPersist], [appContext])}>
+        <div className={styles.container}>
+            {startSession && <Session onEnd={() => setStartSession(false)} />}
+            {!startSession && <HomePage setStartSession={setStartSession} appContext={appContext}/>}
+        </div>
+    </AppContext.Provider>
+}
+
+const calculateScore = (session) => {
+    if(!session || !session.questions){
+        return 0;
+    }
+    let index = 0;
+    let totalCorrect = 0;
+    for (const question of session.questions) {
+        const expectedAnswer = question.reduce((t,n) => t+parseInt(n),0);
+        const answer = session.answers[index];
+        if(expectedAnswer === answer){
+            totalCorrect++;
+        }
+        index++;
+    }
+    return Math.round((totalCorrect / session.questions.length) * 100)
+}
+
+const month = ['JAN','FEB','MAR','APR','MAY','JUN','JUL','AUG','SEP','OCT','NOV','DEC'];
+
+const formatDate = (date) => {
+    return `${date.getDate()}-${month[date.getMonth()]}-${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}`
+}
+function HomePage({setStartSession,appContext}){
+    const [session,setSession] = useState(appContext?.sessions[appContext.sessions.length - 1]);
+    if(session === null || session === undefined){
+        return <div className={styles.homePage}><button className={styles.button} onClick={onStartSession(setStartSession)}>Start Session</button></div>
+    }
+    const indexOfCurrentSessionSelection = appContext.sessions.indexOf(session);
+    const hasNext =  indexOfCurrentSessionSelection < appContext.sessions.length - 1;
+    const hasPrevious = indexOfCurrentSessionSelection > 0
+
+    return <div className={styles.homePage}>
+
+        <div className={styles.buttonContainer}>
+            <button className={styles.buttonPrevNext} disabled={!hasPrevious} onClick={() => setSession(appContext.sessions[indexOfCurrentSessionSelection-1])}>Prev Score</button>
+            <div style={{flex:1}}/>
+            <button className={styles.buttonPrevNext} disabled={!hasNext} onClick={() => setSession(appContext.sessions[indexOfCurrentSessionSelection+1])}>Next Score</button>
+        </div>
+        <div className={styles.title}>
+            <div className={styles.score}>{calculateScore(session)} %</div>
+            <div className={styles.date}>{formatDate(new Date(session.start))}</div>
+        </div>
+        <div className={styles.tableContainer}>
+        <table className={styles.table}>
+            <tr>
+                <th>No</th>
+                <th>Questions</th>
+                <th>Answers</th>
+                <th>Duration</th>
+            </tr>
+            <tbody>
+            {session?.questions?.map((q,index) => {
+                return <tr key={index}>
+                    <td>{index+1}</td>
+                    <td>{q.join(',')}</td>
+                    <td className={styles.tdAnswer}>
+                        <div className={styles.textCenter} >{session.answers[index]}</div>
+                        /
+                        <div className={styles.textCenter}>{session.answers[index]?q.reduce((tot,next) => tot + parseInt(next),0):''}</div>
+                    </td>
+                    <td>{session.durations[index]}</td>
+                </tr>
+            })}
+            </tbody>
+        </table>
+        </div>
+        <button className={styles.button} onClick={onStartSession(setStartSession)}>Start Session</button>
+    </div>
+}
---
+++ src/comp/Session.js
@@ -0,0 +1,177 @@
+import {useContext, useEffect, useRef, useState} from "react";
+import {AppContext} from "./App";
+import styles from "./Session.module.css";
+
+
+const onMounted = (containerRef, setContainerWidth) => () => {
+    return setContainerWidth(containerRef.current.offsetWidth);
+}
+
+const onSessionEnd = (isEnd, onEnd) => () => {
+    if (isEnd) {
+        onEnd();
+    }
+}
+
+export default function Session({onEnd}) {
+    const [appContext, setAppContext] = useContext(AppContext);
+    const [session, setSession] = useState(initiateState(appContext));
+
+    const [activeRowIndex, setActiveRowIndex] = useState(session.answers.length);
+    const [activeColumnIndex, setActiveColumnIndex] = useState(0);
+    const {delayBetweenQuestionsInMilliseconds} = appContext.config;
+    const [containerWidth, setContainerWidth] = useState(800);
+    let {questions} = session;
+    questions = questions || [];
+    const totalQuestions = questions && questions[activeRowIndex] && questions[activeRowIndex].length || 0;
+    useEffect(() => setActiveColumnIndex(0), [activeRowIndex])
+    useEffect(startNewQuestion({
+        activeColumnIndex,
+        setActiveColumnIndex,
+        columnsEachRow: totalQuestions,
+        delayBetweenColumnsInMilliseconds: delayBetweenQuestionsInMilliseconds,
+        activeRowIndex
+    }), [activeColumnIndex, totalQuestions]);
+    const isEnd = session.end.length > 0;
+    const isWaitingForAnswer = activeColumnIndex === totalQuestions && !isEnd;
+    const numberValue = getValue(questions, activeRowIndex, activeColumnIndex);
+    useEffect(onSessionUpdated(setAppContext, session), [session]);
+    const waitingForAnswerRef = useRef(null);
+    useEffect(onWaitingForAnswer(waitingForAnswerRef, isWaitingForAnswer), [isWaitingForAnswer]);
+    const containerRef = useRef();
+    useEffect(onMounted(containerRef, setContainerWidth), []);
+    useEffect(onSessionEnd(isEnd,onEnd), [isEnd]);
+    return <div ref={containerRef} className={styles.container}>
+        {questions.map((q, rowIndex) => {
+            return <div key={rowIndex}>{q.map((n, colIndex) => {
+                return <audio key={`${rowIndex}-${colIndex}`} data-audio={`${rowIndex}-${colIndex}`} >
+                    <source src={`audio/${n}.wav`} type="audio/wav" />
+                    Your browser does not support the audio element.
+                </audio>
+            })}</div>
+        })}
+        <div style={{fontSize: containerWidth * 0.45}}>{numberValue}</div>
+        {isWaitingForAnswer && <Answer containerWidth={containerWidth} onAnswer={(answer) => {
+            if (answer) {
+                setSession(session => {
+                    const answers = session['answers'] || [];
+                    const durations = session['durations'] || [];
+                    answers[activeRowIndex] = answer;
+                    durations[activeRowIndex] = new Date().getTime() - waitingForAnswerRef.current.getTime();
+                    const isEnd = answers.length === session.questions.length;
+                    return {...session, answers, durations, end: isEnd ? new Date().toISOString() : ''};
+                })
+                setActiveRowIndex(activeRowIndex + 1);
+            }
+        }}/>}
+    </div>
+}
+
+function buildQuestions({totalSums, questionsEachSum, digits, negativePercentage}) {
+    let sums = [];
+    while (sums.length < totalSums) {
+        let questions = [];
+        while (questions.length < questionsEachSum) {
+            let number = parseInt((Array.from({length: digits}).map((_, index) => {
+                if (index === 0) {
+                    return Math.ceil(Math.random() * 9)
+                } else {
+                    return Math.floor(Math.random() * 10)
+                }
+            }).join('')));
+
+            const isNegative = Math.ceil(Math.random() * 10) <= (10 * negativePercentage);
+            number = isNegative ? -1 * number : number;
+            const hasSimilarItemInQuestions = questions.indexOf(number) >= 0;
+            const ifTotalIsLesserThanZero = questions.reduce((total, next) => total + next, number) <= 0;
+            if (!hasSimilarItemInQuestions && !ifTotalIsLesserThanZero) {
+                questions.push(number);
+            }
+        }
+        sums.push(questions);
+    }
+    return sums;
+}
+
+
+const startNewQuestion = ({
+                              activeColumnIndex,
+                              setActiveColumnIndex,
+                              columnsEachRow,
+                              delayBetweenColumnsInMilliseconds, activeRowIndex
+                          }) => () => {
+    const audio = document.querySelector(`[data-audio="${activeRowIndex}-${activeColumnIndex}"]`);
+
+    if (audio) {
+        audio.addEventListener('ended', () => {
+            setTimeout(() => {
+                if (activeColumnIndex < columnsEachRow) {
+                    setActiveColumnIndex(activeColumnIndex + 1);
+                }
+            }, delayBetweenColumnsInMilliseconds);
+        })
+        audio.playbackRate = 1.4;
+        audio.play();
+    } else {
+        if (activeColumnIndex < columnsEachRow) {
+            setActiveColumnIndex(activeColumnIndex + 1);
+        }
+    }
+}
+
+
+const getValue = (questions, rowIndex, columnIndex) => {
+    try {
+        return questions[rowIndex][columnIndex]
+    } catch (err) {
+    }
+    return 0;
+}
+
+const onSessionUpdated = (setAppContext, session) => () => {
+    if (session.start) {
+        setAppContext(oldAppContext => {
+            const oldSession = oldAppContext.sessions.filter(s => s.start !== session.start);
+            return {...oldAppContext, sessions: [...oldSession, session]};
+        });
+
+    }
+}
+
+const onWaitingForAnswer = (waitingForAnswerRef, isWaitingForAnswer) => () => {
+    if (isWaitingForAnswer) {
+        waitingForAnswerRef.current = new Date();
+    }
+}
+
+
+const initiateState = (appContext) => () => {
+    const {config, sessions} = appContext;
+    // ok now we have the config lets open last session
+    const unfinishedSessions = sessions.filter(s => 'end' in s && s.end.length === 0);
+    if (unfinishedSessions && unfinishedSessions.length > 0) {
+        return unfinishedSessions[0];
+    } else {
+        return {
+            start: new Date().toISOString(),
+            questions: buildQuestions(config),
+            answers: [],
+            durations: [],
+            end: ''
+        };
+    }
+
+}
+
+
+const handleSubmit = (onAnswer) => (event) => {
+    event.preventDefault();
+    const answer = event.target.elements.answer.value;
+    onAnswer(parseInt(answer));
+}
+
+function Answer({onAnswer, containerWidth}) {
+    return <form onSubmit={handleSubmit(onAnswer)}>
+        <input style={{fontSize: containerWidth * 0.3}} className={styles.answerBox} name={"answer"} autoFocus={true}/>
+    </form>
+}
